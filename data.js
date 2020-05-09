689679f60c19cecea945e8ee59071893352923b4
chiranjibichapagain <chiranjibichapagain@gmail.com>
2020-05-09 14:39:21 +0300

    adding new bookmark

--- data.js
+++ data.js
@@ -1,27 +1,27 @@
 export const bookmarkData = [
   {
     name: "BBC News",
-    Webpage: "https://www.bbc.com/news",
+    webpage: "https://www.bbc.com/news",
     image: "./Assets/bbc.jpg",
   },
   {
     name: "Helsingin Sanomat",
-    Webpage: "https://www.hs.fi",
+    webpage: "https://www.hs.fi",
     image: "./Assets/HS.jpg",
   },
   {
     name: "Vasabladet",
-    Webpage: "https://www.vasabladet.fi/",
+    webpage: "https://www.vasabladet.fi/",
     image: "./Assets/vbl.jpeg",
   },
   {
     name: "FOX News",
-    Webpage: "https://www.foxnews.com/",
+    webpage: "https://www.foxnews.com/",
     image: "./Assets/fox.png",
   },
   {
     name: "The Guardian",
-    Webpage: "https://www.theguardian.com/international",
+    webpage: "https://www.theguardian.com/international",
     image: "./Assets/guardian.jpg",
   },
 ];
--- script.js
+++ script.js
@@ -1,34 +1,57 @@
-// import { bookmarkData } from "./data.js";
-
-const bookmarkData = {
-  name: "",
-  webpage: "",
-};
+import { bookmarkData } from "./data.js";
 
 const getInput = () => {
+  clearData();
   const nameInput = document.getElementById("nameInput").value;
   const webInput = document.getElementById("webInput").value;
-  bookmarkData.name = nameInput;
-  bookmarkData.webpage = webInput;
-  createBookmarks(bookmarkData);
-  nameInput = "";
-  webInput = "";
+
+  if (nameInput === "" || webInput === "") {
+    document.getElementById("instruction").style.opacity = "100%";
+  } else {
+    document.getElementById("instruction").style.opacity = "0%";
+
+    const data = [...bookmarkData];
+    const newData = { name: "", webpage: "", image: "./assets/web.png" };
+    newData.name = nameInput;
+    newData.webpage = webInput;
+    const newArr = data.concat(newData);
+    // console.log(newArr)
+    document.getElementById("nameInput").value = "";
+    document.getElementById("webInput").value = "";
+
+    newArr.map((item) => {
+      createBookmarks(item);
+    });
+  }
 };
+
+function clearData() {
+  document.querySelectorAll(".bookmarkItem").forEach(function (a) {
+    a.remove();
+  });
+}
 
 // function to create a bookmark item consisting img, text and link
 const createBookmarks = (obj) => {
   const bookmarkItem = document.createElement("a");
   bookmarkItem.className = "bookmarkItem";
   bookmarkItem.href = obj.webpage;
+  const icon = document.createElement("img");
+  icon.className = "icon";
+  icon.src = obj.image;
   const pageName = document.createElement("p");
   pageName.className = "pageName";
   pageName.textContent = obj.name;
 
+  bookmarkItem.appendChild(icon);
   bookmarkItem.appendChild(pageName);
   document.getElementById("bookmarks").appendChild(bookmarkItem);
 };
 
+bookmarkData.map((item) => {
+  createBookmarks(item);
+});
+
 // event handlers fro getting input data and submit
-// document.getElementById("nameInput").addEventListener("change", getName);
-// document.getElementById("webInput").addEventListener("change", getWeb);
+
 document.getElementById("button").addEventListener("click", getInput);
