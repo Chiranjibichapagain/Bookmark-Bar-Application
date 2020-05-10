673200a0af0d7f76c40a07b2a9bacc05a692e561
chiranjibichapagain <chiranjibichapagain@gmail.com>
2020-05-10 12:15:39 +0300

    refactor 1

--- data.js
+++ data.js
@@ -2,26 +2,26 @@ export const bookmarkData = [
   {
     name: "BBC News",
     webpage: "https://www.bbc.com/news",
-    image: "./Assets/bbc.jpg",
+    image: "./assets/bbc.jpg",
   },
   {
     name: "Helsingin Sanomat",
     webpage: "https://www.hs.fi",
-    image: "./Assets/HS.jpg",
+    image: "./assets/HS.jpg",
   },
   {
     name: "Vasabladet",
     webpage: "https://www.vasabladet.fi/",
-    image: "./Assets/vbl.jpeg",
+    image: "./assets/vbl.jpeg",
   },
   {
     name: "FOX News",
     webpage: "https://www.foxnews.com/",
-    image: "./Assets/fox.png",
+    image: "./assets/fox.png",
   },
   {
     name: "The Guardian",
     webpage: "https://www.theguardian.com/international",
-    image: "./Assets/guardian.jpg",
+    image: "./assets/guardian.jpg",
   },
 ];
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ bookmarkData.map((item) => {
   createBookmarks(item);
 });
 
-const copyArr = [...bookmarkData]; // an array to add a new obj that comes form the input, and to be used for rearranging items.
+let copyArr = [...bookmarkData]; // an array to add a new obj that comes form the input, and to be used for rearranging items.
 
 const getInput = () => {
   const nameInput = document.getElementById("nameInput").value;
@@ -15,34 +15,38 @@ const getInput = () => {
   if (nameInput === "" || webInput === "") {
     document.getElementById("instruction").style.opacity = "100%";
   } else {
-    document.getElementById("instruction").style.opacity = "0%";
-    clearData(); // to prevent adding new set of items while old exists in the UI, function is created below
+    addBookmark(copyArr, nameInput, webInput, createBookmarks)
 
-    // creating a new object from data inserted form input
-    const data = [...bookmarkData];
-    const newData = { name: "", webpage: "", image: "./Assets/xxx.png" };
-    newData.name = nameInput;
-    newData.webpage = webInput;
-    copyArr.push(newData);
-    const newArr = data.concat(newData);
-
-    document.getElementById("nameInput").value = "";
-    document.getElementById("webInput").value = "";
-
-    newArr.map((item) => {
-      createBookmarks(item);
-    });
   }
 };
 
-// clearing up function
+const addBookmark = (arr, name, link, callback) => {
+  document.getElementById("instruction").style.opacity = "0%";
+  clearData();
+
+  // creating a new object from data inserted form input
+  const newData = {
+    name: name,
+    webpage: link,
+    image: "./assets/xxx.png",
+  };
+  arr = [...arr, newData];
+  document.getElementById("nameInput").value = "";
+  document.getElementById("webInput").value = "";
+
+  arr.map((item) => {
+    callback(item);
+  });
+};
+
+// To prevent adding new set of items while old exists in the UI.
 function clearData() {
   document.querySelectorAll(".bookmarkItem").forEach(function (a) {
     a.remove();
   });
 }
 
-// rearranging function
+// Rearranging function
 const rearrange = () => {
   clearData();
   const sortedArr = copyArr.sort(() => {
@@ -53,8 +57,7 @@ const rearrange = () => {
     createBookmarks(item);
   });
 };
-rearrange();
 
-// event handlers fro getting input data and submit, and rearranging
+// Event handlers fro getting input data and submit, and rearranging
 document.getElementById("button").addEventListener("click", getInput);
 document.getElementById("suffle").addEventListener("click", rearrange);
