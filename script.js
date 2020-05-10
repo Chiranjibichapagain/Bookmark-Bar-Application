1b08bceaea4d50e250ebaf154c8072ae6650ded4
chiranjibichapagain <chiranjibichapagain@gmail.com>
2020-05-10 13:34:24 +0300

    styles

--- script.js
+++ script.js
@@ -5,28 +5,20 @@ bookmarkData.map((item) => {
   createBookmarks(item);
 });
 
-let copyArr = [...bookmarkData]; // an array to add a new obj that comes form the input, and to be used for rearranging items.
+let copyArr = [...bookmarkData]; // a new array to include new obj that comes form the addBookmarkfunction.
 
 const getInput = () => {
   const nameInput = document.getElementById("name-input").value;
   const webInput = document.getElementById("web-input").value;
-  // making sure that empty items will not be added, rather displays an error
+  // if input field is empty we give error message else we create a new obj and display..
   nameInput === "" || webInput === ""
     ? (document.getElementById("errorMessage").style.opacity = "100%")
     : addBookmark(nameInput, webInput, createBookmarks);
 };
 
-console.log("copy arr,", copyArr);
-/**
- *
- * @param {1 array} arr Description: list of bookrmarks
- * @param {2 string} name  Bookmark name
- * @param {3 strng} link
- * @param {4 callbackfunction} createBookmark
- */
 const addBookmark = (name, link, createBookmark) => {
-  document.getElementById("errorMessage").style.opacity = "0%";
-  clearData();
+  document.getElementById("errorMessage").style.opacity = "0%"; // hiding back the message
+  clearData(); // clearing bookmarks to send new list including new bookmark
 
   // creating a new object from data inserted form input
   const newData = {
@@ -34,7 +26,6 @@ const addBookmark = (name, link, createBookmark) => {
     webpage: link,
     image: "./assets/xxx.png",
   };
-
   copyArr = [...copyArr, newData];
 
   document.getElementById("name-input").value = "";
